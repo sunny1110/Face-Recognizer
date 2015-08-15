645b2f8a6f5e06b024dbd20a0cd6de1e4475e888
Prithvi <prithvi.prakash@gmail.com>
2015-08-15 15:54:44 +0530

    All Errors Fixed

--- classifier.py
+++ classifier.py
@@ -1,21 +1,20 @@
 #import the necessary packages from the import time
 
 
-import matplot.pyplot as plt
+import matplotlib.pyplot as plt
 import numpy as np
 
 #import the machine learning packages
 from sklearn.datasets import fetch_lfw_people
-frin sklearn.cross_validation import train_test_split
+from sklearn.cross_validation import train_test_split
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import classsification_report
+from sklearn.metrics import classification_report
 from sklearn.metrics import confusion_matrix
 
 # Downloading the data.
 # From the servers and if the data is alredy present.
 # In the current working directory load them as numpy array
-
-lfw_people = fetch_lfw_people(min_faces_per_person = 70)
+lfw_people = fetch_lfw_people( min_faces_per_person = 70)
 
 
 n_samples, h, w = lfw_people.images.shape
@@ -31,13 +30,13 @@ y = lfw_people.target # Target Variable
 n_images = X.shape[0] # Number of Images
 n_features = X.shape[1] # Number of Features
 person_name = lfw_people.target_names # Name of the person in the images
-
+n_classes = person_name.shape[0]
 print person_name
 
 # printing the information about the dataset
 
 print "Total DataSet Size: \n"
 print "Number of Images: %d" %n_images
-print "Number of Features: %d" % n_features
-print "Number of Classes: %d" n_classes
+print "Number of Features: %d" %n_features
+print "Number of Classes: %d" %n_classes
 
